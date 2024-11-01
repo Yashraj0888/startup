4fbc0d3dc622f7427211143752f6490feea8e02c
Yashraj0888 <bloodymoonknite888@gmail.com>
2024-11-02 00:41:33 +0530

    env

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,22 +1,23 @@
 export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-10-28';
 
-export const dataset = assertValue(
-  process.env.SANITY_STUDIO_DATASET || 'default-dataset', // Provide a default
-  'Missing environment variable: SANITY_STUDIO_DATASET'
-);
-
+  export const dataset = assertValue(
+    process.env.SANITY_STUDIO_DATASET || process.env.SANITY_STUDIO_DATASET,
+    'Missing environment variable: SANITY_STUDIO_DATASET or NEXT_PUBLIC_SANITY_STUDIO_DATASET'
+  )
+  
 export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
-);
-
-export const token = process.env.SANITY_WRITE_TOKEN;
+    process.env.SANITY_STUDIO_PROJECT_ID || process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    'Missing environment variable: SANITY_STUDIO_PROJECT_ID or NEXT_PUBLIC_SANITY_PROJECT_ID'
+  )
+  
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
   if (v === undefined) {
-    throw new Error(errorMessage);
+    throw new Error(errorMessage)
+   
+
   }
 
-  return v;
+  return v
 }
