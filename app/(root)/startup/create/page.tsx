9859a5c39de7bfc53584f82d68c612d6a35733ff
Yashraj0888 <bloodymoonknite888@gmail.com>
2024-11-03 21:54:37 +0530

    Updates

--- app/(root)/startup/create/page.tsx
+++ app/(root)/startup/create/page.tsx
@@ -1,5 +1,5 @@
 import { auth } from '@/auth'
-import StartupForm from '@/components/StartupForm'
+import StartupForm from '@/Components/StartupForm'
 
 import { redirect } from 'next/navigation'
 import React from 'react'
